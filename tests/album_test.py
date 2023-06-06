8bc5325e83b469fd17d4de0bc04423ed29763e9e
Mark Slorach <hello@markslorach.com>
2023-06-06 21:20:49 +0100

    run tests to check artist class and album class

--- tests/album_test.py
+++ tests/album_test.py
@@ -4,4 +4,13 @@ from models.album import Album
 class TestAlbum(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.album = Album("The Dark Side of the Moon", "Rock", "Pink Floyd")
+    
+    def test_album_has_title(self):
+        self.assertEqual("The Dark Side of the Moon", self.album.title)
+    
+    def test_album_has_genre(self):
+        self.assertEqual("Rock", self.album.genre)
+
+    def test_album_has_artist(self):
+        self.assertEqual("Pink Floyd", self.album.artist)
--- tests/artist_test.py
+++ tests/artist_test.py
@@ -4,4 +4,7 @@ from models.artist import Artist
 class TestArtist(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.artist = Artist("Pink Floyd")
+    
+    def test_artist_has_name(self):
+        self.assertEqual("Pink Floyd", self.artist.name)
