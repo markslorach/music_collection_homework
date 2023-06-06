7a50d2c8b222c901eb56cb2b16a25a0576bc09cf
Mark Slorach <hello@markslorach.com>
2023-06-06 22:11:21 +0100

    added save artist

--- console.py
+++ console.py
@@ -3,3 +3,10 @@ from models.album import Album
 import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 from models.artist import Artist
+
+pdb.set_trace()
+artist_1 = Artist("The Beatles")
+artist_repository.save(artist_1)
+
+
+
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -2,3 +2,12 @@ import pdb
 from db.run_sql import run_sql
 from models.artist import Artist
 import repositories.album_repository as album_repository
+
+def save(artist):
+    sql = "INSERT INTO artists (artist_name) VALUES (%s) RETURNING *"
+    values = [artist.artist_name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    artist.id = id
+    return artist
+
