ef5fac488b5d4895a403e178cb5a0e56ddb18164
Mark Slorach <hello@markslorach.com>
2023-06-06 21:22:29 +0100

    added import code to console.py

---
+++ console.py
@@ -0,0 +1,5 @@
+import pdb
+from models.album import Album
+import repositories.album_repository as album_repository
+import repositories.artist_repository as artist_repository
+from models.artist import Artist
